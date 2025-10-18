35b424907221565b50cea09d5454247d4478460d
thealeskerov <161497084+thealeskerov@users.noreply.github.com>
2025-10-18 08:34:40 +0200

    Add user input for name, age, and color

---
+++ Exercise3.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+color = input("Enter your favorite color: ")
+
+print(f"Hi {name}! You are {age} years old and you like {color}.")
