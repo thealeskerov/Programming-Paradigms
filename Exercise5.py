5626a7ed30e8e155770ecc460cbafb2ebc61cbc2
thealeskerov <161497084+thealeskerov@users.noreply.github.com>
2025-10-18 12:37:49 +0200

    Add basic arithmetic operations in Exercise5.py

---
+++ Exercise5.py
@@ -0,0 +1,9 @@
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+
+print("Sum:", a + b)
+print("Difference:", a - b)
+print("Product:", a * b)
+print("Quotient:", a / b)
+print("Remainder:", a % b)
+print("Power:", a ** b)
